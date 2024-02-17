891a35d3c81f132cdd1bf8167bc96ab9581ed46f
Karan Kurani <85026744+kuranikaran@users.noreply.github.com>
2024-02-17 01:30:38 -0500

    Update loop in the assembly.c

--- CYB-633/loop in the assembly.c
+++ CYB-633/loop in the assembly.c
@@ -1,6 +1,8 @@
-for (int i = 10; i > 0; i--) {
-    if (i % 2 == 0) {
-        printf("oh my\n");
-    }
+for (int i = 10; i > 0; i--) 
+{
+        if (i % 2 == 0) 
+        {
+            printf("oh my\n");
+        }
 }
 printf("Karan");
